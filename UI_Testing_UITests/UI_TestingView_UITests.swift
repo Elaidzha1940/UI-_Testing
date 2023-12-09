769b4706ca628704c0c604427d4408f7d128b367
Elaidzha1940 <eli080293@gmail.com>
2023-12-09 14:52:21 +0300

    Update UI_TestingView_UITests

--- UI_Testing_UITests/UI_TestingView_UITests.swift
+++ UI_Testing_UITests/UI_TestingView_UITests.swift
@@ -9,6 +9,10 @@
 
 import XCTest
 
+// Naming Structure: test_Unit_OfWork_State_Under_Test_ExpectedBehavior
+// Naming Structure: test_[struct]_[ui component]_[expected result]
+// Testing Structure: Given, When, Then
+
 final class UI_TestingView_UITests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -16,6 +20,14 @@ final class UI_TestingView_UITests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
+        
+    }
+    
+    func test_UI_TestingView_saveButton_shoudNotSaveIt() {
+        
+    }
+    
+    func test_UI_TestingView_saveButton_shoudSaveIt() {
         
     }
 
