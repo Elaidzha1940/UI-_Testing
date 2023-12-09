1a1f2a8d83c7a1a66cec1a71fed32d020d4181e2
Elaidzha1940 <eli080293@gmail.com>
2023-12-09 09:46:50 +0300

    UI_Testing

---
+++ UI_Testing/UI_TestingApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: UI_Testing
+//  File: UI_TestingApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct UI_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
