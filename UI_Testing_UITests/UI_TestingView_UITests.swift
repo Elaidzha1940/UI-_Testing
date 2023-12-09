eeeb70faad1a8d9936af238c28de0f125c8edf73
Elaidzha1940 <eli080293@gmail.com>
2023-12-09 12:46:21 +0300

    Add a new Target

---
+++ UI_Testing_UITests/UI_TestingView_UITests.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: UI_Testing_UITests
+//  File: UI_TestingView_UITests.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.12.2023
+//
+//  */
+
+import XCTest
+
+final class UI_TestingView_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+}
