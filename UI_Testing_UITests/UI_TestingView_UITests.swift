56d6f4a21e0a439eff165ef70bccc44fd8d964f6
Elaidzha1940 <eli080293@gmail.com>
2023-12-10 15:37:43 +0300

    Update UI_TestingView

--- UI_Testing_UITests/UI_TestingView_UITests.swift
+++ UI_Testing_UITests/UI_TestingView_UITests.swift
@@ -71,7 +71,34 @@ final class UI_TestingView_UITests: XCTestCase {
     }
     
     func test_SavedHomeView_showAlertButton_shouldDisplayAlert() {
+        // Given
+        let textfield = app.textFields["SaveTextField"]
         
+        // When
+        textfield.tap()
+        
+        let keyE = app.keys["E"]
+        keyE.tap()
+        let keye = app.keys["e"]
+        keye.tap()
+        keye.tap()
+        
+        let returnButton = app.buttons["Return"]
+        returnButton.tap()
+        
+        let saveButton = app.buttons["SaveButton"]
+        saveButton.tap()
+        
+        let navBar =  app.navigationBars["Welcome to the store"]
+        XCTAssertTrue(navBar.exists)
+        
+        let showAlertButton = app.buttons["ShowAlertButton"]
+        showAlertButton.tap()
+        
+        let alert = app.alerts.firstMatch
+        
+        // Then
+        XCTAssertTrue(alert.exists)
     }
     
     func test_SavedHomeView_showAlertButton_shouldDisplayAndDismissAlert() {
@@ -103,12 +130,45 @@ final class UI_TestingView_UITests: XCTestCase {
         XCTAssertTrue(alert.exists)
 
         let alertOKButton = alert.buttons["OK"]
-        XCTAssertTrue(alertOKButton.exists)
-        sleep(1)
+        //XCTAssertTrue(alertOKButton.exists)
+        
+        //sleep(1)
+        let alertOKButtonExists = alertOKButton.waitForExistence(timeout: 5)
+        XCTAssertTrue(alertOKButtonExists)
+        
         alertOKButton.tap()
-        sleep(1)
+        
+        //sleep(1)
+        let alertExists = alert.waitForExistence(timeout: 5)
     
         // Then
+        XCTAssertFalse(alertExists)
         XCTAssertFalse(alert.exists)
+    }
+    
+    func test_SavedHomeView_navigationLinkToDestination_shouldNavigateToDestination() {
+        
+    }
+    
+    func test_SavedHomeView_navigationLinkToDestination_shouldNavigateToDestinationAndGoBack() {
+        
+        let app = app2
+        app/*@START_MENU_TOKEN@*/.textFields["SaveTextField"]/*[[".textFields[\"Add an item here...\"]",".textFields[\"SaveTextField\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let app2 = app
+        app2/*@START_MENU_TOKEN@*/.keys["E"]/*[[".keyboards.keys[\"E\"]",".keys[\"E\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let eKey = app2/*@START_MENU_TOKEN@*/.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        eKey.tap()
+        eKey.tap()
+        eKey.tap()
+        eKey.tap()
+        app2/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["SaveButton"]/*[[".buttons[\"Save\"]",".buttons[\"SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["NavigationLinkToDestination"]/*[[".buttons[\"Navigate\"]",".buttons[\"NavigationLinkToDestination\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.staticTexts["Destination"].tap()
+        app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["Welcome to the store"].tap()
+        app.navigationBars["Welcome to the store"].staticTexts["Welcome to the store"].tap()
+                 
     }
 }
