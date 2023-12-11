61790ecfdadb3430e0442ab687e199c263a1a29c
Elaidzha1940 <eli080293@gmail.com>
2023-12-11 11:54:57 +0300

    Update UI_TestingApp / Edited Scheme

--- UI_Testing/UI_TestingApp.swift
+++ UI_Testing/UI_TestingApp.swift
@@ -11,6 +11,13 @@ import SwiftUI
 
 @main
 struct UI_TestingApp: App {
+    
+    init() {
+        
+        let itemIsSaved: Bool = CommandLine.arguments.contains("-UITest_startSaving") ? true : false
+       print("ITEM IS SAVED \(itemIsSaved)")
+    }
+    
     var body: some Scene {
         WindowGroup {
             UI_TestingView()
